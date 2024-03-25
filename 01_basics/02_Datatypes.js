a09e546781e7784143a835d690b2a898985a5420
praveenvicky <praveengouda04@gmail.com>
2024-03-25 06:24:49 +0530

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //to treat all js code as newer versions
+
+//alert(3 + 3); //We are using node js not browser
+
+/*
+types- 1) primitives:
+               i)Number = 100;
+               ii)String= ""
+               iii)boolean= either true/false
+               iv)undefined= value not defined 
+               eg.. let a;
+                    console.log(a); //undefined
+                v)null = //represent empty value
+                eg... let string=null
+                      console.log(string); // null
+                vi)Symbol= used for uniquness
+         
+        2)non-primitive/refrence:
+                i)object=        
+
+
+  */

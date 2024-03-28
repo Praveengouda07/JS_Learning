abdcd915c07e98ff0c1582e7036d841eff2bf678
praveenvicky <praveengouda04@gmail.com>
2024-03-28 15:42:50 +0530

    dataconversion

--- 01_basics/03_DatatypeConversion.js
+++ 01_basics/03_DatatypeConversion.js
@@ -1,3 +1,49 @@
-let score=33;
+//Number To String conversion
+let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-console.log(typeof score);
+let scoreInNumber = Number(score);
+// console.log(scoreInNumber);
+// console.log(typeof  scoreInNumber);
+
+
+//String To Number conversion
+let value="33abc"
+
+let newValue = Number(value);
+// console.log(newValue);
+// console.log(typeof newValue);
+
+
+//String to number
+let oldName="33";
+let newName = String(oldName);
+//console.log(newName);
+
+// Boolean to number
+let isLogged = "true";
+let booleanIsLogged = Number(isLogged);
+//console.log(booleanIsLogged);
+
+//number to boolean
+let newLogged = "true";
+//let booleanNewLogged = boolean(newLogged);
+//console.log(booleanIsLogged);
+
+
+//***********Operators**************
+
+//Arithmetic Operators
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+
+let a = 16
+let b = 3
+console.log (a%b);
+
+console.log(++a);
+console.log(a++);
